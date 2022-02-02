d0f8c7bf48e0fede5044e57aab6d6dfc2d11f28e
martycodes333 <84352695+martycodes333@users.noreply.github.com>
2022-02-02 10:06:11 -0800

    TS

---
+++ moving_average_signals.ts
@@ -0,0 +1,29 @@
+# Six EMA x NINE EMA crossovers
+
+input longEntries = yes;
+input shortEntries = no;
+
+input fastMALength = 6;
+input slowMALength = 9;
+input averageTypeFast = AverageType.Exponential;
+input averageTypeSlow = AverageType.Exponential;
+
+
+input quantity = 500;
+
+plot fastMA = MovingAverage (averageTypeFast, close, fastMALength);
+plot slowMA = MovingAverage (averageTypeSlow, close, slowMALength);
+
+
+#6 EMA x 9 EMA 
+# AddLabel
+
+
+
+AddOrder(OrderType.Buy_To_Open, (longEntries) and fastMA crosses above slowMA, close, quantity);
+AddOrder(OrderType.Sell_To_Close, fastMA crosses below slowMA, close, quantity); 
+
+#AddOrder(OrderType.Sell_To_Open, (shortEntries) and fastMA crosses below slowMA, close, quantity);
+#AddOrder(OrderType.Buy_To_Close, fastMA crosses above slowMA, close, quantity); 
+
+#AddOrder(OrderType.BUY_AUTO, no) ;
